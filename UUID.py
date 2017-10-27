b93f418341e136709218ccf44d96f91fd8dc70f5
globalaplication <32783197+globalaplication@users.noreply.github.com>
2017-10-27 14:26:08 +0300

    Update UUID.py

--- UUID.py
+++ UUID.py
@@ -2,7 +2,6 @@
 #!/usr/bin/env python3
 import subprocess
 import os
-
 Qstring, fsStr, dict, uid_list, no_repeat = '', '', {}, [], []
 def cutstring(string, referans, start='"', end='"'):
     return string[string.find(start, string.find(referans)+len(referans))+len(start):string.find(end,
@@ -14,18 +13,20 @@ proc = subprocess.Popen(['sudo', 'blkid', '-c', '/dev/null'],stdout=subprocess.P
 for ij in range(0, 11, +1):
     line = proc.stdout.readline()
     if len(line.rstrip()) is not 0:
+        san = str(line.rstrip())
+        sandisk = san[0:san.find(':')]
         uid, typ  = cutstring(str(line.rstrip()), 'UUID') + ' ' + '/media/' + cutstring(str(line.rstrip()), 'UUID') , cutstring(str(line.rstrip()), 'TYPE')
-        dict[ij] = {'UUID':uid, 'TYPE':typ}
+        dict[ij] = {'sandisk':sandisk, 'UUID':uid, 'TYPE':typ}
 for pt in range(0, len(dict), +1):
-    if fsStr.find(dict[pt]['UUID'][0]) is -1:
+    if fsStr.find(dict[pt]['UUID'].split(' ')[1]) is -1 and fsStr.find(dict[pt]['UUID'].split(' ')[0]) is -1:
         if dict[pt]['TYPE'] == 'vfat':
-            uid, typ, ect = 'UUID=' + '"' + dict[pt]['UUID'] + '"', '' + '"' + dict[pt]['TYPE'] + '"', 'rw,auto,user,fmask=0111,dmask=0000,noatime,nodiratime 0 0'
+            uid, typ, ect = dict[pt]['sandisk'] + ' ' + dict[pt]['UUID'].split(' ')[1], dict[pt]['TYPE'], 'rw,auto,user,fmask=0111,dmask=0000,noatime,nodiratime 0 0'
             Qstring = Qstring + uid + ' ' + typ + ' ' + ect + '\n'
         if dict[pt]['TYPE'] == 'ext4':
-            uid, typ, ect = 'UUID=' + '"' + dict[pt]['UUID'] + '"', '' + '"' + dict[pt]['TYPE'] + '"', 'defaults 0 0'
+            uid, typ, ect = dict[pt]['sandisk'] + ' ' + dict[pt]['UUID'].split(' ')[1], dict[pt]['TYPE'], 'defaults 0 0'
             Qstring = Qstring + uid + ' ' + typ + ' ' + ect + '\n'
 os.system('sudo chmod +777 /etc/fstab')
-fsStr = fsStr + '\n' + Qstring.replace('"','')
+fsStr = fsStr + '\n' + Qstring.replace('"','').replace("b'","")
 with open('/etc/fstab', "w") as beta: 
     beta.write(str(fsStr)) 
 os.system('sudo chmod 604 /etc/fstab')
